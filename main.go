30dc8c41157196f4ea53f6efa84c1f19fb90a22f
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-09-09 17:30:19 +0900

    フォームに入力してログインできるようにする。

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/sclevine/agouti"
-	"log"
+	"time"
 )
 
 func main() {
@@ -21,4 +21,33 @@ func main() {
 		fmt.Printf("Failed to open a new page. %s\n", err)
 		return
 	}
+
+	// todo: コマンドラインから出勤と退勤を選択する
+
+	// AKASHIログインページ移動
+	if err := page.Navigate("https://atnd.ak4.jp/login?next=%2Fmypage%2Fpunch"); err != nil {
+		fmt.Printf("Failed to navigate to AKASHI login page. %s\n", err)
+		return
+	}
+
+	// フォームに内容を入力して、ログインを行う
+	formCompanyId := page.FindByID("form_company_id")
+	formLoginId := page.FindByID("form_login_id")
+	formPassword := page.FindByID("form_password")
+
+	formCompanyId.Fill("company")
+	formLoginId.Fill("login_id")
+	formPassword.Fill("password")
+
+	// todo: 後で消す（テスト用スリープ、入力内容確認）
+	time.Sleep(2 * time.Second)
+
+	formSubmitButton := page.FindByID("submit-button")
+	if err := formSubmitButton.Click(); err != nil {
+		fmt.Printf("Failed to navigate to form button click. %s\n", err)
+		return
+	}
+
+	// todo: 後で消す（テスト用スリープ、入力内容確認）
+	time.Sleep(2 * time.Second)
 }
