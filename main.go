a3aabafe780ce5af2038911005debeb436949278
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-09-09 17:11:37 +0900

    ページ起動までのエラーハンドリング追加

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sclevine/agouti"
 	"log"
 )
 
 func main() {
+	// chromeドライバを使って、Google Chromeを使用できるようにする。
 	agoutiDriver := agouti.ChromeDriver()
-	agoutiDriver.Start()
+	if err := agoutiDriver.Start(); err != nil {
+		fmt.Printf("Failed to start driver. %s\n", err)
+		return
+	}
 	defer agoutiDriver.Stop()
-	page, _ := agoutiDriver.NewPage()
+
+	// agoutiを使用して新規ブラウザを立ち上げる。このブラウザを使って色々操作します。
+	page, err := agoutiDriver.NewPage()
+	if err != nil {
+		fmt.Printf("Failed to open a new page. %s\n", err)
+		return
+	}
 }
